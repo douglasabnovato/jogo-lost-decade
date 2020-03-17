9c1b1ee340429f96934c3b26884c576bc76df134
@douglasabnovato <douglasabnovato@gmail.com>
2020-03-16 23:47:09 -0300

    canvas

--- game.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// criar o canvas
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext('2d');
-canvas.width = 512;
-canvas.height = 480;
-document.body.appendChild(canvas);
--- gameBabel.js
+++ gameBabel.js
@@ -4,3 +4,18 @@ const ctx = canvas.getContext('2d');
 canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
+
+// imagem de fundo
+let bgReady = false;
+const bgImage = new Image();
+bgImage.onload = function(){
+    bgReady = true;
+};
+bgImage.src = 'images/background.jpg';
+
+//imagem do herói
+let heroReady = false;
+const heroImage = new Image();
+heroImage.onload = function(){
+    heroImage = true;
+};
